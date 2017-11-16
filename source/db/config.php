ed6e9992c17b2faeee51ecdc0d7de6844a19ff9e
Edgar <edandporras@gmail.com>
2017-11-15 22:52:31 -0500

    app

--- source/db/config.php
+++ source/db/config.php
@@ -2,6 +2,6 @@
 	define('db_server', 'localhost:3306');
 	define('db_user', 'mapache');
 	define('db_pass', 'teleco2017');
-	define('db_db', 'mapache');
-	$db = mysqli_connect(db_server, db_user, db_pass, db_db);
+	define('db_name', 'mapache');
+	$db = mysqli_connect(db_server, db_user, db_pass, db_name);
 ?>
